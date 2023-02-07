d01c95149570ead1672da79b5d69a10026a0bf03
JorgeCadrecha <113858332+JorgeCadrecha@users.noreply.github.com>
2023-02-07 12:20:30 +0000

    Ejercicios

---
+++ ejercicio1.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+           int a,b;
+           a=3;
+           System.out.println("a = " + a );
+           //Se impreme el valor de a que es 3 en este caso
+           b=a+5;
+           System.out.println("a = " + a + " y b = " + b);
+              //Se imprime el valor de a que es 3 y el valor de b que es 8
+           a=7;
+           System.out.println("a = " + a + " y b = " + b);
+           //Se imprime el valor de a que es 7 y el valor de b que es 8
+    }
+}
+
+
